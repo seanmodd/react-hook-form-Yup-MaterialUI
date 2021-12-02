9f25b6d79933ab50919083bdf3ad266dd37b8f21
seanmodd <sean@vestaridefair.com>
2021-12-02 12:13:30 -0800

    Purely Vin

--- src/App.js
+++ src/App.js
@@ -10,15 +10,14 @@ import {
   TextField,
   Typography,
   FormControlLabel,
-  Checkbox,
   Button,
 } from '@material-ui/core';
 
 const App = () => {
   const validationSchema = Yup.object().shape({
-    username: Yup.string()
-      .required('Username is required')
-      .matches('[A-HJ-NPR-Z0-9]{17}', 'Is not in correct format'),
+    vin: Yup.string()
+      .required('Vin is required')
+      .matches('[A-HJ-NPR-Z0-9]{17}', 'Vin not in correct format'),
   });
 
   const {
@@ -46,47 +45,18 @@ const App = () => {
             <Grid item xs={12} sm={6}>
               <TextField
                 required
-                id="username"
-                name="username"
-                label="Username"
+                id="vin"
+                name="vin"
+                label="Vin"
                 fullWidth
                 margin="dense"
-                {...register('username')}
-                error={errors.username ? true : false}
+                {...register('vin')}
+                error={errors.vin ? true : false}
               />
               <Typography variant="inherit" color="textSecondary">
-                {errors.username?.message}
+                {errors.vin?.message}
               </Typography>
             </Grid>
-            {/* <Grid item xs={12}>
-              <FormControlLabel
-                control={
-                  <Controller
-                    control={control}
-                    name="acceptTerms"
-                    defaultValue="false"
-                    inputRef={register()}
-                    render={({ field: { onChange } }) => (
-                      <Checkbox
-                        color="primary"
-                        onChange={(e) => onChange(e.target.checked)}
-                      />
-                    )}
-                  />
-                }
-                label={
-                  <Typography color={errors.acceptTerms ? 'error' : 'inherit'}>
-                    I have read and agree to the Terms *
-                  </Typography>
-                }
-              />
-              <br />
-              <Typography variant="inherit" color="textSecondary">
-                {errors.acceptTerms
-                  ? '(' + errors.acceptTerms.message + ')'
-                  : ''}
-              </Typography>
-            </Grid> */}
           </Grid>
 
           <Box mt={3}>
