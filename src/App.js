35eb95af21b8a7088974fffcaefc646300fe9020
seanmodd <sean@vestaridefair.com>
2021-12-02 11:40:20 -0800

    Now just username and terms

--- src/App.js
+++ src/App.js
@@ -45,21 +45,6 @@ const App = () => {
           </Typography>
 
           <Grid container spacing={1}>
-            <Grid item xs={12} sm={12}>
-              <TextField
-                required
-                id="fullname"
-                name="fullname"
-                label="Full Name"
-                fullWidth
-                margin="dense"
-                {...register('fullname')}
-                error={errors.fullname ? true : false}
-              />
-              <Typography variant="inherit" color="textSecondary">
-                {errors.fullname?.message}
-              </Typography>
-            </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
                 required
@@ -73,53 +58,6 @@ const App = () => {
               />
               <Typography variant="inherit" color="textSecondary">
                 {errors.username?.message}
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="email"
-                name="email"
-                label="Email"
-                fullWidth
-                margin="dense"
-                {...register('email')}
-                error={errors.email ? true : false}
-              />
-              <Typography variant="inherit" color="textSecondary">
-                {errors.email?.message}
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="password"
-                name="password"
-                label="Password"
-                type="password"
-                fullWidth
-                margin="dense"
-                {...register('password')}
-                error={errors.password ? true : false}
-              />
-              <Typography variant="inherit" color="textSecondary">
-                {errors.password?.message}
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                id="confirmPassword"
-                name="confirmPassword"
-                label="Confirm Password"
-                type="password"
-                fullWidth
-                margin="dense"
-                {...register('confirmPassword')}
-                error={errors.confirmPassword ? true : false}
-              />
-              <Typography variant="inherit" color="textSecondary">
-                {errors.confirmPassword?.message}
               </Typography>
             </Grid>
             <Grid item xs={12}>
